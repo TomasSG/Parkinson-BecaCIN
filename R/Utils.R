619fdf2a3e6cea6b83afa0fd5e51ef8b392ddee9
TomasSG <tomassanchezgrigioni98@gmail.com>
2021-01-12 19:38:42 -0300

    Estoy armando la regresión logística

--- R/Utils.R
+++ R/Utils.R
@@ -4,5 +4,10 @@ contar_ids_repetidos <- function(data, id){
   data %>% dplyr::count({{id}}) %>% dplyr::filter(n > 1) %>% nrow()
 }
 
+armar_tabla_frecuencia <- function(data, vars){
+  data %>% 
+    select(all_of(vars)) %>% 
+    map(table)
+}
 
 
