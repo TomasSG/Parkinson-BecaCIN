9b90b6e933f2c7f842a6c7434321c9b89c7e81da
TomasSG <tomassanchezgrigioni98@gmail.com>
2021-01-17 17:22:55 -0300

    Hice un random forest con tunning para practicar y ver que tal

--- R/Utils.R
+++ R/Utils.R
@@ -10,4 +10,10 @@ armar_tabla_frecuencia <- function(data, vars){
     map(table)
 }
 
+evaluar_modelo <- function(modelo, datos_test, nivel_positivo, var_respuesta) {
+  predicciones <- predict(modelo, datos_test, list = FALSE, type = "prob")[,nivel_positivo]
+  auc <- roc(datos_test[[var_respuesta]], predicciones) %>% auc
+  return(auc)
+}
+
 
