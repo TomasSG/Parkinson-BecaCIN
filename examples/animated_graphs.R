653754c0963ac7566f9275e654d2b7509cf10250
TomasSG <tomassanchezgrigioni98@gmail.com>
2021-01-22 22:42:27 -0300

    Encontre como hacer la animación

--- examples/animated_graphs.R
+++ examples/animated_graphs.R
@@ -3,6 +3,7 @@
 # Bilbiotecas
 library(gganimate)
 library(tidyverse)
+library(gifski)
 
 
 # Cargamos los datos
@@ -12,6 +13,11 @@ glimpse(datos)
 
 # Vamos a probar de hacer la proporción de personas con parkinsons según edad
 
+# Declaramos unas variables que van a ser importantes al final
+N_FRAME <-  120
+HEIGHT <- 700
+WIDTH <- 1000
+
 # Preparamos los datos
 prop_positivos_por_edad <- datos %>% 
   mutate(diagnostico_pro = ifelse(diagnostico_pro == "true", 1, 0)) %>% 
@@ -19,18 +25,25 @@ prop_positivos_por_edad <- datos %>%
   summarise(prop_pos = sum(diagnostico_pro) / n()) 
 
 # Hacemos el gráfico estático
-g <- ggplot(prop_positivos_por_edad, aes(edad, prop_pos)) + 
-  geom_point() +
-  geom_line() +
+grafico <- ggplot(prop_positivos_por_edad, aes(edad, prop_pos)) + 
+  geom_point(color = "firebrick") +
+  geom_line(color = "firebrick") +
   xlab("Edad") +
   ylab("Proporción de positivos") +
-  ggtitle("Relación postiiva entre edad y la proproción de positivos", 
+  ggtitle("Relación positiva entre edad y la proproción de positivos", 
           "Al aumentar la edad se tiene una tendencia de tener mayor proporción de positivos") +
-  theme_minimal()
+  theme_bw()
 
 # Lo hacemos dinámico
-g + transition_reveal(edad)
+animacion <- grafico + transition_reveal(edad)
 
+animate(p, 
+        render = gifski_renderer("./examples/render_edad_prop_pos.gif"), 
+        nframe = N_FRAME, 
+        height = HEIGHT, 
+        width = WIDTH)
+
+# TODO: Por qué tengo que usar gifski?
 
 
 
