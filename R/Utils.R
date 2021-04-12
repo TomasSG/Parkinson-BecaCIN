ab983408cacd73d3e0321c759ea7751c282cc048
TomasSG <tomassanchezgrigioni98@gmail.com>
2021-04-12 16:09:26 -0300

    Hice función para armar df con los datos que me piden

--- R/Utils.R
+++ R/Utils.R
@@ -1,19 +1,29 @@
 # --------------------------------------Funciones------------------------------------
 
 contar_ids_repetidos <- function(data, id){
+  if(require(tidyverse) == FALSE){
+    return()
+  }
   data %>% dplyr::count({{id}}) %>% dplyr::filter(n > 1) %>% nrow()
 }
 
 armar_tabla_frecuencia <- function(data, vars){
+  if(require(tidyverse) == FALSE){
+    return()
+  }
+  
   data %>% 
     select(all_of(vars)) %>% 
     map(table)
 }
 
 graficar_curva_roc <- function(modelo, datos_test, nivel_positivo, var_respuesta, type_predict = "prob") {
+  if(require(pROC) == FALSE || require(tidyverse) == FALSE){
+    return()
+  }
   
   # Primero obtenemos las probabilidades predichas
-  predicciones <- predict(modelo, datos_test, list = FALSE, type = type_predict)[,nivel_positivo]
+  predicciones <- predict(modelo, datos_test, type = type_predict)[,nivel_positivo]
   
   # Calculamos la sensitivity y especificity para cada valor de corte posible
   objeto_roc <- roc(datos_test[[var_respuesta]], predicciones)
@@ -29,4 +39,22 @@ graficar_curva_roc <- function(modelo, datos_test, nivel_positivo, var_respuesta
     ggtitle("Curva ROC", paste("AUROC:", round(auroc, 4)))
 }
 
-
+crear_df_evaluacion <- function(modelo, 
+                                datos, 
+                                nombre_var_respuesta, 
+                                ids, 
+                                type_predict = "prob", 
+                                nivel_positivo = "true") {
+  
+  if(require(tidyverse) == FALSE){
+    return()
+  }
+  
+  y_predichos <- predict(modelo, datos, type = type_predict)[,nivel_positivo]
+  
+  y_obs <- datos[[nombre_var_respuesta]]
+  
+  df_resultado <- data.frame(ids, y_obs, y_predichos) %>% arrange(y_predichos)
+  
+  return(df_resultado)
+}
