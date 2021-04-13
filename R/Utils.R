459108c057b3d271c6bcbd6aee306f6c1804c848
TomasSG <tomassanchezgrigioni98@gmail.com>
2021-04-13 09:45:09 -0300

    Hecha funćión para evaluar el modelo

--- R/Utils.R
+++ R/Utils.R
@@ -1,7 +1,10 @@
+MSJ_ERROR_BIBLIOTECAS <- "Faltan bibliotecas necesarias"
+
 # --------------------------------------Funciones------------------------------------
 
 contar_ids_repetidos <- function(data, id){
   if(require(tidyverse) == FALSE){
+    print(MSJ_ERROR_BIBLIOTECAS)
     return()
   }
   data %>% dplyr::count({{id}}) %>% dplyr::filter(n > 1) %>% nrow()
@@ -9,6 +12,7 @@ contar_ids_repetidos <- function(data, id){
 
 armar_tabla_frecuencia <- function(data, vars){
   if(require(tidyverse) == FALSE){
+    print(MSJ_ERROR_BIBLIOTECAS)
     return()
   }
   
@@ -19,6 +23,7 @@ armar_tabla_frecuencia <- function(data, vars){
 
 graficar_curva_roc <- function(modelo, datos_test, nivel_positivo, var_respuesta, type_predict = "prob") {
   if(require(pROC) == FALSE || require(tidyverse) == FALSE){
+    print(MSJ_ERROR_BIBLIOTECAS)
     return()
   }
   
@@ -39,26 +44,45 @@ graficar_curva_roc <- function(modelo, datos_test, nivel_positivo, var_respuesta
     ggtitle("Curva ROC", paste("AUROC:", round(auroc, 4)))
 }
 
-crear_df_evaluacion <- function(modelo, 
-                                datos, 
-                                nombre_var_respuesta, 
-                                nombre_var_ids, 
-                                type_predict = "prob", 
-                                nivel_positivo = "true") {
+calcular_porcentaje_correctos <- function(modelo, datos, p_df, nombre_var_respuesta, nombre_var_ids, type_predict = "prob", nivel_positivo = "true") {
   
   if(require(tidyverse) == FALSE){
+    print(MSJ_ERROR_BIBLIOTECAS)
     return()
-  }
+  }  
   
-  y_predichos <- predict(modelo, datos, type = type_predict)[,nivel_positivo]
+  # Obtenemos el df para hacer la evaluacion
+  df <- crear_df_evaluacion(modelo, datos, nombre_var_respuesta, nombre_var_ids, type_predict, nivel_positivo)
   
-  y_obs <- datos[[nombre_var_respuesta]]
+  # Calculamos las n observaciones que vamos a usar en base a p_df.
+  nro_filas_df <- nrow(df)
+  nro_filas_sub <- floor(p_df * nro_filas_df)
   
-  ids <- datos[[nombre_var_ids]]
+  # Apartamos las primeras n observaciones y calculamos el porcentajes de correctos
+  df <- df[1:nro_filas_sub,]
+  p_correctos <- sum(df$y_obs == nivel_positivo) * 100 / nro_filas_sub
   
-  df_resultado <- data.frame(ids, y_obs, y_predichos) %>% arrange(-y_predichos)
+  return(round(p_correctos, 2))
   
-  return(df_resultado)
 }
 
-calcular_porcentaje_correctos <- function()
+crear_df_evaluacion <- function(modelo, datos, nombre_var_respuesta, nombre_var_ids, type_predict, nivel_positivo ) {
+  
+  if(require(tidyverse) == FALSE){
+    print(MSJ_ERROR_BIBLIOTECAS)
+    return()
+  }  
+  
+  # Hacemos la predicción
+  y_predichos <- predict(modelo, datos, type = type_predict)[,nivel_positivo]
+  
+  # Extraemos los valores y observados
+  y_obs <- datos[[nombre_var_respuesta]]
+  
+  # Extraemos los ids
+  ids <- datos[[nombre_var_ids]]
+  
+  # Creamos el df y devolvemos el resultado
+  df_resultado <- data.frame(ids, y_obs, y_predichos) %>% arrange(-y_predichos)
+  return(df_resultado)
+}
